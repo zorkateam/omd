154bf5ad6eaad9883a6ee3da64a7c44434ccc9c3
zorkateam <zorkateam@gmail.com>
2021-10-26 10:16:51 +0300

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -5,7 +5,6 @@ class CountVectorizer:
     features - список уникальных слов в корпусе текстов
     count_matrix - Терм-документная матрица
     corpus - исходный корпус текстов
-
     Методы:
     fit_transform - формируем матрицу с количеством вхождений слов из features в корпус текстов
     get_feature_names - возвращает список уникальных слов из корпуса текстов
@@ -26,14 +25,15 @@ class CountVectorizer:
             string_ = text.lower().split(' ')
             self.corpus.append(string_)
             for word in string_:
-                if word not in self.features:
+                if word and word not in self.features:
                     self.features.append(word)
         # Заполняем count_matrix
         self.count_matrix = []
         for row_number, text in enumerate(self.corpus):
             self.count_matrix.append([])
             for _, word in enumerate(self.features):
-                self.count_matrix[row_number].append(text.count(word))
+                if word:
+                    self.count_matrix[row_number].append(text.count(word))
         return self.count_matrix
 
     def get_feature_names(self):
@@ -64,37 +64,46 @@ def make_tests() -> bool:
     Проводим тесты методов класса CountVectorizer
     :return: bool
     """
-    tests = [{'input': ['Crock Pot Pasta Never boil pasta again', 'Pasta Pomodoro Fresh ingredients Parmesan to taste'],
-              'term_matrix': [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]],
-              'features': ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro', 'fresh', 'ingredients',
-                           'parmesan', 'to', 'taste']},
+    tests = {1: {'input': ['Crock Pot Pasta Never boil pasta again', 'Pasta Pomodoro Fresh ingredients '
+                           'Parmesan to taste'],
+                 'term_matrix': [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]],
+                 'features': ['crock', 'pot', 'pasta', 'never', 'boil', 'again', 'pomodoro', 'fresh', 'ingredients',
+                              'parmesan', 'to', 'taste']},
 
-             {'input': ['Читайте классику снова и снова',
-                        'Пушкина Демидовича Толстого',
-                        'Решайте за завтраком решайте перед сном',
-                        'Когда вас положат после сессии в дурдом'],
+             2: {'input': ['0', '', 'a', ''],
+                 'term_matrix': [[1, 0], [0, 0], [0, 1], [0, 0]],
+                 'features': ['0', 'a']},
 
-              'term_matrix': [[1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                              [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                              [0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-                              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]],
-              'features': ['читайте', 'классику', 'снова', 'и', 'пушкина', 'демидовича', 'толстого', 'решайте', 'за',
-                           'завтраком', 'перед', 'сном', 'когда', 'вас', 'положат', 'после', 'сессии', 'в', 'дурдом']}
-             ]
+             3: {'input': ['', '', '', ''],
+                 'term_matrix': [[], [], [], []],
+                 'features': []},
 
-    for test in tests:
+             4: {'input': ['Читайте классику снова и снова',
+                           'Пушкина Демидовича Толстого',
+                           'Решайте за завтраком решайте перед сном',
+                           'Когда вас положат после сессии в дурдом'],
+                 'term_matrix': [[1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]],
+                 'features': ['читайте', 'классику', 'снова', 'и', 'пушкина', 'демидовича', 'толстого',
+                              'решайте', 'за', 'завтраком', 'перед', 'сном', 'когда', 'вас', 'положат',
+                              'после', 'сессии', 'в', 'дурдом']}
+             }
+
+    for test_number, test in tests.items():
         vectrzr = CountVectorizer()
         term_matrix = vectrzr.fit_transform(test['input'])
         features = vectrzr.get_feature_names()
         for row_term_matrix, row_test_matrix in zip(term_matrix, test['term_matrix']):
-            assert is_equal_lists(row_term_matrix, list(row_test_matrix)), 'Ошибка в term_matrix'
-        assert is_equal_lists(features, test['features']), 'Ошибка в списке features'
+            assert is_equal_lists(row_term_matrix, list(row_test_matrix)), f'Тест {test_number}. Ошибка в term_matrix'
+        assert is_equal_lists(features, test['features']), f'Тест {test_number}. Ошибка в списке features'
     return True
 
 
 if __name__ == "__main__":
     if make_tests():
-        print('Тесты пройдены!')
+        print('Все тесты пройдены!')
 
     new_text = ['Семь бед один ответ',
                 'Костыль и велосипед',
